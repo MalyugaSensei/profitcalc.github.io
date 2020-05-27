dccf9596b9004296147d813bfcf284c6ce852926
MalyugaSensei <31480571+MalyugaSensei@users.noreply.github.com>
2020-05-27 15:44:33 +0500

    upload and update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var firstPrice = 0, secondPrice = 0, profitPerc = 0, profitUSD = 0, profitRUB = 0, rate = 0, Solution = 0, money = 0, mode = 0, i = 0, usd, eur;
+var mode, firstPrice = 0, secondPrice = 0, profitPerc = 0, profitUSD = 0, profitRUB = 0, rate = 0, Solution = 0, money = 0, mode = 0, i = 0, usd, eur;
 
 i = 1;
 
@@ -11,17 +11,18 @@ rate = $.getJSON("https://www.cbr-xml-daily.ru/daily_json.js",
     }
 );
 
-$('.option-buy').on('click', (e) => {
+$('.option').on('click', function (e) {
     e.preventDefault();
     $('.change-options').hide();
-    $('.change-area').show();
-    $('.text-message').html('Выберите площадку');
+    var option = $(this).attr('id');
+    mode = option;
+    changeOption(option);
 });
 
 $('.service-option').on('click', function (e) {
     e.preventDefault();
-    $('.change-area').hide();
-    var servType = $(this).attr('data-service-type');
+    $('.change-serv').hide();
+    var servType = $(this).attr('id');
     changeService(servType);
 });
 
@@ -29,55 +30,89 @@ $('.deposit-option').on( 'click', function (e) {
     e.preventDefault();
     $('.change-deposit').hide();
     $('.input-price').show();
-    $('.price-area').before('Цена на сайте');
+    $('.price-serv').before('Цена на сайте');
     $('.price-any').before('Цена в стиме');
     $('.text-message').html('Введите цены');
     var depType = $(this).attr('data-deposit-type');
     $('.calc').attr('data-deposit-type', depType);
 });
 
+
 $('.calc').on('click', function (e) {
     e.preventDefault();
-    firstPrice = $('.price-area').val();
+    console.log(mode);
+    firstPrice = $('.price-serv').val();
     secondPrice = $('.price-any').val();
+    console.log($(this).attr('data-service-type'));
     switch ($(this).attr('data-service-type')) {
         case "dmarket" :
             var depType = $(this).attr('data-deposit-type');
-            
-            if ( depType != undefined ) {
-                dmarket(depType, firstPrice, secondPrice);
-            }
+            dmarket(depType, firstPrice, secondPrice, mode);
             break;
         case "steam" :
-
+            steam(firstPrice, secondPrice, mode);
             break;
         case "dm-csanal" :
-            dmarkCSanal(firstPrice, secondPrice);
+            dmarkCSanal(firstPrice, secondPrice, mode);
             break;
         case "waxpeer" :
 
             break;        
     }
-
-
-
 });
 
-function changeService (servType) {
+function changeOption(option) {
+    switch (option) {
+        case "buy" :
+            $('.text-message').html('Выберите площадку');
+            $('.change-serv').show();
+            break;
+        case "auto-buy" :
+            $('.text-message').html('Выберите площадку');
+            $('.change-serv').show();
+            $('.change-serv #waxpeer').hide();
+            break;
+    }
+}
+
+function changeService (servType, mode) {
     switch (servType) {
         case "dmarket" :
-            $('.change-deposit').show();
-            $('.text-message').html('Выберите способ пополнения');
+            if (mode == "buy") {
+                $('.text-message').html('Выберите способ пополнения');
+                $('.change-deposit').show();
+            } else {
+                $('.input-price').show();
+                $('.text-message').html('Введите цены');
+                $('.price-serv').before('Цена в стиме');
+                $('.price-any').hide();
+            }
             $('.calc').attr('data-service-type', servType);
             break;
         case "steam" :
-
+            $('.input-price').show();
+            if (mode == "buy") {
+                $('.text-message').html('Введите цены');
+                $('.price-serv').before('Цена покупки');
+                $('.price-any').before('Цена продажи');
+            } else {
+                $('.text-message').html('Введите цены');
+                $('.price-serv').before('Цена в стиме');
+                $('.price-any').hide();
+            }
+            $('.calc').attr('data-service-type', servType);
             break;
         case "dm-csanal" :
             $('.input-price').show();
-            $('.price-area').before('Цена на сайте');
-            $('.price-any').before('Цена в аналисте');
-            $('.text-message').html('Введите цены');
+            if (mode == "buy") {
+                $('.text-message').html('Введите цены');
+                $('.price-serv').before('Цена покупки');
+                $('.price-any').before('Цена продажи');
+            } else {
+                $('.text-message').html('Введите цены');
+                $('.price-serv').before('Цена в analyst');
+                $('.price-any').hide();
+            }
             $('.calc').attr('data-service-type', servType);
             break;
         case "waxpeer" :
@@ -86,7 +121,7 @@ function changeService (servType) {
     }
 }
 
-function resultOutput (result) {
+function resultOutput (result, profitPerc) {
     $('.text-message').html(result);
 
     if (profitPerc > 60) {
@@ -103,41 +138,85 @@ function resultOutput (result) {
     }
 }
 
-function dmarket(depType, firstPrice, secondPrice) {
-    var firstPriceUSD, firstPriceEUR;
-    firstPriceUSD = firstPrice * usd;
-    firstPriceEUR = firstPrice * eur;
-    if ( depType == 'visa' ) {
-        firstPriceUSD = firstPriceUSD / 0.975 + usd * 0.33;   
-        firstPriceEUR = firstPriceEUR / 0.975 + eur * 0.33;
+function dmarket(depType, firstPrice, secondPrice, mode) {
+    var firstPriceUSD, firstPriceEUR, profitArr = new Array();
+    switch (mode) {
+        case "buy" :
+            firstPriceUSD = firstPrice * usd;
+            firstPriceEUR = firstPrice * eur;
+            if ( depType == 'visa' ) {
+                firstPriceUSD = firstPriceUSD / 0.975 + usd * 0.33;   
+                firstPriceEUR = firstPriceEUR / 0.975 + eur * 0.33;
+            }
+            if ( depType == 'bitcoin' ) {
+                firstPriceUSD = firstPriceUSD / 0.99 + usd * 0.06;   
+                firstPriceEUR = firstPriceEUR / 0.99 + eur * 0.06;
+            }
+            secondPrice = secondPrice * 0.85;
+            profitRUB = secondPrice;
+            profitPerc = (secondPrice / firstPriceUSD - 1) * 100;
+            secondPrice = secondPrice - firstPriceUSD;
+            profitUSD = secondPrice / usd;
+            resultOutput("Профит " + Math.round(profitPerc) + "% Профит " + Math.round(secondPrice) + "Руб. Профит $" + Math.round(profitUSD) + " Заплатили " + Math.round(firstPriceUSD) + "Руб. Получили в Steam " + Math.round(profitRUB) + "Руб.", profitPerc);
+            break;
+        case "auto-buy" :
+            firstPrice = firstPrice / usd;
+            firstPrice = firstPrice * 0.85;
+            secondPrice = firstPrice / 1.3;
+            console.log(profitArr);
+            profitArr.push('Профит 30% ' + Math.round(secondPrice));
+            secondPrice = firstPrice / 1.5;
+            profitArr.push('Профит 50% ' + Math.round(secondPrice));
+            secondPrice = firstPrice / 1.7;
+            profitArr.push('Профит 70% ' + Math.round(secondPrice));
+            resultOutput(profitArr);
+            break;
     }
-    if ( depType == 'bitcoin' ) {
-        firstPriceUSD = firstPriceUSD / 0.99 + usd * 0.06;   
-        firstPriceEUR = firstPriceEUR / 0.99 + eur * 0.06;
+
+}
+
+function steam (firstPrice, secondPrice, mode) {
+    var profitArr = new Array();;
+    switch (mode) {
+        case "buy" :
+            secondPrice = secondPrice * 0.85;
+            profitPerc = (secondPrice / firstPrice - 1) * 100;
+            secondPrice = secondPrice - firstPrice;
+            resultOutput("Профит " + Math.round(profitPerc) + "% Профит " + Math.round(secondPrice) + "Руб");
+            break;
+        case "auto-buy" :
+            firstPrice = firstPrice * 0.85;
+            secondPrice = firstPrice / 1.3;
+            profitArr.push('Профит 30% ' + Math.round(secondPrice));
+            secondPrice = firstPrice / 1.5;
+            profitArr.push('Профит 50% ' + Math.round(secondPrice));
+            secondPrice = firstPrice / 1.7;
+            profitArr.push('Профит 70% ' + Math.round(secondPrice));
+            resultOutput(profitArr);
+            break;
     }
-    secondPrice = secondPrice * 0.85;
-    profitRUB = secondPrice;
-    profitPerc = (secondPrice / firstPriceUSD - 1) * 100;
-    secondPrice = secondPrice - firstPriceUSD;
-    profitUSD = secondPrice / usd;
-    resultOutput("Профит " + Math.round(profitPerc) + "% Профит " + Math.round(secondPrice) + "Руб. Профит $" + Math.round(profitUSD) + " Заплатили " + Math.round(firstPriceUSD) + "Руб. Получили в Steam " + Math.round(profitRUB) + "Руб.")
-}
-
-
-function dmarkCSanal (firstPrice, secondPrice) {
-    secondPrice = secondPrice * 0.85;
-    profitPerc = (secondPrice / firstPrice - 1) * 100;
-    resultOutput("Профит " + profitPerc + "%");
 
 }
 
-function steam (firstPrice, secondPrice) { 
-    secondPrice = secondPrice * 0.85;
-    profitPerc = (secondPrice / firstPrice - 1) * 100;
-    secondPrice = secondPrice - firstPrice;
-    resultOutput("Профит " + profitPerc + "% Профит " + secondPrice + "Руб");
+function dmarkCSanal (firstPrice, secondPrice, mode) {
+    var profitArr = new Array();
+    switch (mode) {
+        case "buy" :
+            secondPrice = secondPrice * 0.85;
+            profitPerc = (secondPrice / firstPrice - 1) * 100;
+            resultOutput("Профит " + Math.round(profitPerc) + "%", Math.round(profitPerc));
+            break;
+        case "auto-buy" :
+            firstPrice = firstPrice * 0.85;
+            secondPrice = firstPrice / 1.3;
+            profitArr.push('Профит 30% ' + Math.round(secondPrice));
+            secondPrice = firstPrice / 1.5;
+            profitArr.push('Профит 50% ' + Math.round(secondPrice));
+            secondPrice = firstPrice / 1.7;
+            profitArr.push('Профит 70% ' + Math.round(secondPrice));
+            resultOutput(profitArr);
+            break;
+    }
+
+
 }
-
-
-
-
